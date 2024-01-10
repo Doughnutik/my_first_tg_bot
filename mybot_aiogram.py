d186491878f56b9dbd17b7d59b20b8e78513b969
Doughnutik <artemgukovwork@yandex.ru>
2024-01-10 15:30:52 +0300

    поменял readme, начал писать mybot_aiogram

--- mybot_aiogram.py
+++ mybot_aiogram.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher, types, executor
+from aiogram.types.web_app_info import WebAppInfo
 import config
 
 bot = Bot(config.BOT_TOKEN)
@@ -6,26 +7,9 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.reply('И тебе привет')
-    file = open('meme_with_black.jpg', 'rb')
-    await message.answer_photo(file)
-
-@dp.message_handler(content_types=['voice'])
-async def into(message: types.Message):
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    markup.add(types.InlineKeyboardButton('Site', url='https://ya.ru'))
-    markup.add(types.InlineKeyboardButton('Hello', callback_data='hello'))
-    await message.reply('Hello', reply_markup=markup)
-    
-@dp.callback_query_handler()
-async def callback(call):
-    await call.message.answer(call.data)
-
-@dp.message_handler(commands=['reply'])
-async def reply(message):
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.add(types.KeyboardButton('Site'))
-    markup.add(types.KeyboardButton('Website'))
-    await message.answer('Hello', reply_markup=markup)
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton('Открыть гитхаб бота', web_app=WebAppInfo(url='https://github.com/Doughnutik/my_first_tg_bot')))
+    markup.add(types.KeyboardButton('Открыть мой сайт', web_app=WebAppInfo(url='https://index.html')))
+    await message.answer('Привет', reply_markup=markup) 
     
 executor.start_polling(dp)
